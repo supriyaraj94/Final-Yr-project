aeb0eef7992047ff99e4bcc3b0d3f66fcb0a6479
supriyaraj94 <supriyaraj47@gmail.com>
2016-03-08 08:53:56 +0530

    finding null_space using GA

--- quadratic_sieve/main.py
+++ quadratic_sieve/main.py
@@ -1,7 +1,8 @@
 import math
-from prime_generator import primesfrom2to
+from primesieve import *
 from sympy import *
 from trial1 import *
+from fractions import gcd
 
 
 def get_smoothness_factor(n):
@@ -33,6 +34,7 @@ def generate_sequence(a,b,n):
     return seq
         
 def filter_b_smooth(old_seq,primes):
+    old_seq.sort()
     seq_copy=old_seq[:]
     factors=dict()
     for ele in seq_copy:
@@ -53,7 +55,7 @@ def filter_b_smooth(old_seq,primes):
                 old_seq.pop(i)
             else:
                 old_seq[i]=num
-            i+=1            
+                i+=1                     
     return smooth_seq            
 
 def compute_a(seq,n):
@@ -66,12 +68,16 @@ def compute_a(seq,n):
         factors.append([x%2 for x in seq[ele]])
     return a ,b,factors     
 
+def write_matrix_to_file(m):
+  filehandle = open("null_matrix.txt", "w")
+  filehandle.write(str(m));
+  filehandle.close()
 
 n=int(input("Enter n to factorize"))
 b_smooth=get_smoothness_factor(n)
 #b_smooth=43
 #print(b_smooth)
-prime_list=primesfrom2to(b_smooth)
+prime_list=generate_primes(2,b_smooth)
 factor_base=determine_factor_base(prime_list,n)
 #print(factor_base)
 Xsqr_minus_N_sequence=generate_sequence(int(math.sqrt(n)),int(math.sqrt(2*n)),n)
@@ -82,12 +88,17 @@ b_smooth_sequence=filter_b_smooth(Xsqr_minus_N_sequence,factor_base)
 #print(len(b_smooth_sequence))
 #Finding the null space
 a_base,b_base,factor_matrix=compute_a(b_smooth_sequence,n)
+#print(())
+write_matrix_to_file(factor_matrix)
+'''
+#print((factor_matrix))
 sympy_mat=Matrix(factor_matrix).T
-print(sympy_mat.rref())
-print(b_base)
-pivotal_numbers=sympy_mat.rref()[1]
+echelon=sympy_mat.rref();
+#print(echelon)
+#print(b_base)
+pivotal_numbers=echelon[1]
 #print(pivotal_numbers)
-
+#print("Pivots:"+str(len(pivotal_numbers)))
 new_a_base=[a_base[x] for x in pivotal_numbers]
 new_b_base=[b_base[x] for x in pivotal_numbers]
 new_factor_matrix=[factor_matrix[x] for x in pivotal_numbers]
@@ -95,7 +106,8 @@ new_factor_matrix=[factor_matrix[x] for x in pivotal_numbers]
 #print(new_a_base)
 b = np.array(new_factor_matrix).transpose()
 bsol = GF2inv(b)
-print(b)
+#print(bsol)
+#print(new_b_base)
 
 
 soln_vectors=[]
@@ -109,11 +121,15 @@ for vector in bsol[1]:
             pro_a*=new_a_base[i]
             for j in range(len(vector)):
                 check[j]=(check[j] + new_factor_matrix[i][j])%2
-    print(check)
-    print(pro_b)
-    print(pro_a)            
-
-
+    if(sum(check)==0):
+        #print(check)
+        #print(pro_b)
+        #print(pro_a)            
+        a=math.sqrt(pro_b)
+        b=pro_a
+        #print(gcd(a+b,n))
+        #print(gcd(a-b,n))
+'''
 
 
 
---
+++ quadratic_sieve/null_space_ga.py
@@ -0,0 +1,75 @@
+from pyevolve import G1DList
+from pyevolve import GSimpleGA
+from pyevolve import GAllele
+from pyevolve import Mutators
+from pyevolve import Initializators
+from pyevolve import Crossovers
+from pyevolve import Consts
+import sys, random
+from math import sqrt
+
+
+matrix=[]
+prime_length=0
+num_length=0
+
+def cartesian_matrix(file):
+   """ Read the matrix from the file """
+   m=[]
+   line=file.read()
+   return (eval(line))
+
+
+def eval_func(chromosome):
+   """ The evaluation function """
+   global matrix
+   global num_length
+   global prime_length
+   s=[0 for x in range(prime_length)]
+   for i in range(len(chromosome)):
+      if(chromosome[i]==1):
+         s=[(x + y)%2 for x, y in zip(s, matrix[i])] 
+   #print(sum(s))           
+   return sum(s)
+  
+
+   
+def main_run():
+   global matrix
+   global prime_length
+   global num_length
+   # load the tsp data file
+   filehandle = open("null_matrix.txt", "rw")
+   matrix = cartesian_matrix(filehandle)
+   prime_length=len(matrix[0])
+   num_length=len(matrix)
+
+   
+   # set the alleles
+   setOfAlleles = GAllele.GAlleles()
+   for i in xrange(num_length):
+      a = GAllele.GAlleleRange(0, 1)
+      setOfAlleles.add(a)
+      
+   genome = G1DList.G1DList(num_length)
+   genome.setParams(allele=setOfAlleles)
+   genome.setParams(bestrawscore=0, rounddecimal=2)
+
+   genome.evaluator.set(eval_func)
+   genome.mutator.set(Mutators.G1DListMutatorAllele)
+   genome.initializator.set(Initializators.G1DListInitializatorAllele)
+   
+   ga = GSimpleGA.GSimpleGA(genome)
+   ga.setGenerations(1000)
+   ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
+   ga.setMinimax(Consts.minimaxType["minimize"])
+   ga.setCrossoverRate(1.0)
+   ga.setMutationRate(0.03)
+   ga.setPopulationSize(80)
+
+   ga.initialize()
+   ga.evolve( freq_stats = 100)
+   print( eval_func(ga.bestIndividual() ))
+
+if __name__ == "__main__":
+   main_run()
