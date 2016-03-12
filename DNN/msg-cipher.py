855109f449f3b2627f5bd94d229977c51d780e8b
SairaKhanum <sairakhanumf@gmail.com>
2016-03-12 22:04:26 +0530

    Updated

--- DNN/msg-cipher.py
+++ DNN/msg-cipher.py
@@ -1,7 +1,7 @@
 # This script takes input and output filename as command line arguments
 # Example: python msg_cipher.py inpfile outfile
 
-import rsa, sys
+import rsa, sys, cPickle as pickle
 from rsa.bigfile import *
 
 from rsa import key, common, pkcs1, varblock
@@ -14,22 +14,21 @@ with open('public.pem', mode='rb') as publicfile:
 	keydata = publicfile.read()
 pub_key = rsa.PublicKey.load_pkcs1(keydata)
 
+with open('private.pem', mode='rb') as privatefile:
+	keydata = privatefile.read()
+priv_key = rsa.PrivateKey.load_pkcs1(keydata)
+
 with open(inpfile, 'rb') as infile, open(outfile, 'wb') as oufile:
 	encrypt_bigfile(infile, oufile, pub_key)
 
-key_bytes = common.bit_size(pub_key.n)
-blocksize = key_bytes - 11 # keep space for PKCS#1 padding
-
 messages = []
 ciphers = []
-with open(inpfile, 'rb') as infile, open(outfile, 'rb') as oufile:
-	for block in varblock.yield_fixedblocks(infile, blocksize):
-		messages.append(block)
-
+with open(outfile, 'rb') as oufile:
 	for block in varblock.yield_varblocks(oufile):
+		cleartext = pkcs1.decrypt(block, priv_key)
+		messages.append(cleartext)
 		ciphers.append(block)
 
-print len(messages), len(ciphers)
-
-training_data = zip(messages, ciphers)
-print training_data
+training_dataset = (messages, ciphers)
+with open("m2c_training_dataset.p", "wb") as f:
+	pickle.dump(training_dataset, f)
