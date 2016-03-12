0e91dc9d93a0685f6b25a599166176aabb02d479
SairaKhanum <sairakhanumf@gmail.com>
2016-03-12 17:55:33 +0530

    Training Dataset Generation

--- DNN/msg_cipher.py
+++ DNN/msg_cipher.py
@@ -1,8 +1,11 @@
 # This script takes input and output filename as command line arguments
 # Example: python msg_cipher.py inpfile outfile
-'''
+
 import rsa, sys
 from rsa.bigfile import *
+
+from rsa import key, common, pkcs1, varblock
+from rsa._compat import byte
 
 inpfile = sys.argv[1]
 outfile = sys.argv[2]
@@ -13,9 +16,18 @@ pub_key = rsa.PublicKey.load_pkcs1(keydata)
 
 with open(inpfile, 'rb') as infile, open(outfile, 'wb') as outfile:
 	encrypt_bigfile(infile, outfile, pub_key)
-'''
 
-from rsa import key, common, pkcs1, varblock
-from rsa._compat import byte
+key_bytes = common.bit_size(pub_key.n)
+blocksize = key_bytes - 11 # keep space for PKCS#1 padding
 
-# Working on this....
+messages = []
+for block in varblock.yield_fixedblocks(inpfile, blocksize):
+	messages.append(block)
+
+ciphers = []
+for block in varblock.yield_varblocks(outfile):
+	ciphers.append(block)
+
+for x,y in zip(messages, ciphers):
+	print x, "---------------------", y
+	print "================================================================================="
