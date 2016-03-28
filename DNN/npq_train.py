10f1fbeccd9bd6865c2ae8d72dde000852ec8d2d
SairaKhanum <sairakhanumf@gmail.com>
2016-03-28 19:19:15 +0530

    modified

--- DNN/npq_train.py
+++ DNN/npq_train.py
@@ -16,7 +16,8 @@ tar = tar.reshape(len(tar), num_output_units)
 
 trans = [nl.trans.TanSig()] * (len(size) - 1) + [nl.trans.LogSig()]
 # Create network with n layers
-net = nl.net.newff(minmax, size, transf=trans)
+#net = nl.net.newff(minmax, size, transf=trans)
+net = nl.load('npq-model-10000.net')
 
 # Change traning func, by default uses train_bfgs
 #net.trainf = nl.train.train_gd
